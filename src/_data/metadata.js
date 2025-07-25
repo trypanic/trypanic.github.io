4ab3bf340494a620999857c157cb294ca44b21d1
trypanic <116321356+trypanic@users.noreply.github.com>
2025-07-25 11:43:26 -0500

    feat: add setting files

---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "trypanic blog",
+	url: "https://trypanic.github.io",
+	language: "en",
+	description: "A software engineering and computer science blog where I break things out of curiosity — to reinvent the wheel, to understand things in depth, or just for fun. And if it all goes wrong… well, that’s why it’s called Trypanic (as in trying, or entering panic 😄).",
+	author: {
+		name: "David Nuñez",
+		email: "",
+		url: "https://github.com/trypanic"
+	}
+};
